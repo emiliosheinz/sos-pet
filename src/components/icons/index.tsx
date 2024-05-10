de16b106eb6ed53f520aef54d3a6edcc5acb5d01
Guilherme Rigotti <ggrigotti@gmail.com>
2024-05-10 17:05:02 -0300

    feat: Add search input component and icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -57,8 +57,29 @@ function InstagramIcon() {
   );
 }
 
+function SearchIcon(props: { className: string }) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <path d="m21 21-4.3-4.3" />
+    </svg>
+  );
+}
+
 export const Icons = {
   XIcon,
   FacebookIcon,
   InstagramIcon,
+  SearchIcon,
 };
