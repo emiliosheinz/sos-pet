a88e49a7300a8f0371eb4e75282c9e5f1d9fa06e
Guilherme Rigotti <ggrigotti@gmail.com>
2024-05-10 20:52:30 -0300

    feat: Update search input and filters icons
    The search input and filters components now use different icons from the `react-icons` library. The search input now uses the `CiSearch` icon, and the filters component now uses the `CiFilter` and `FaChevronDown` icons. This update improves the visual consistency and enhances the user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,7 @@ export default function Home() {
   const { data: session } = useSession();
   const { data } = api.shelter.findAll.useQuery();
 
+  console.log(data);
   // return (
   //   <main className="flex w-full flex-col  items-center justify-center gap-2 bg-white pt-16">
   //     {!!session && <h2>{session.user.name}</h2>}
@@ -50,20 +51,6 @@ export default function Home() {
   return (
     <main className="flex w-full flex-col  items-center justify-center gap-2 bg-white pt-16">
       {!!session && <h2>{session.user.name}</h2>}
-      <ul>
-        {data?.map((shelter) => (
-          <li key={shelter.id} className="rounded-md border-2 border-black p-2">
-            <div className="flex flex-col">
-              <span>Name: {shelter.name}</span>
-              <span>Phone: {shelter.phone}</span>
-              <span>Capacity: {shelter.capacity}</span>
-              <span>Occupancy: {shelter.occupancy}</span>
-              <span>Donations: {shelter.donations}</span>
-              <span>Volunteers: {shelter.volunteers}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
       {!!session ? (
         <Button onClick={() => signOut()}>Sign out</Button>
       ) : (
@@ -74,7 +61,8 @@ export default function Home() {
         <SearchInput />
         <Filters menus={menus} />
       </div>
-      <Card />
+
+      {data?.map((shelter) => <Card key={shelter.id} shelter={shelter} />)}
     </main>
   );
 }
