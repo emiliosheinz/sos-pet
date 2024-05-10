681f36bbd54ab411289cdbd4ea22c192506d3509
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-05-09 21:30:11 -0300

    feat: initialize chadcnui

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -21,12 +21,7 @@ export const postRouter = createTRPCRouter({
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
-        },
-      });
+      return [];
     }),
 
   getLatest: protectedProcedure.query(({ ctx }) => {
