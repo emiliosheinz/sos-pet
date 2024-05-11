5e8960cebde87f7f4c0f36ed90a72b87b851f7db
William Moraes <willmoraes.ev@gmail.com>
2024-05-11 15:16:30 -0300

    refactor: :fire: remove unecessary layouts

--- src/app/privacy-policy/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default async function PrivacyPolicy({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
--- src/app/terms-of-use/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default async function TermsOfUse({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
