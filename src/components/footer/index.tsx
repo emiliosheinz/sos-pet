76677f35d01806132bf76acb485cf85a3e20701a
William Moraes <willmoraes.ev@gmail.com>
2024-05-11 20:02:31 -0300

    feat: :sparkles: add instagram link on footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { FaInstagram } from "react-icons/fa";
 
 export function Footer() {
   return (
-    <div className="mt-8 flex w-full justify-center border-t-2 border-slate-300/25 bg-inherit px-4 py-8 align-middle xl:px-0">
+    <footer className="flex w-full justify-center border-t-2 border-slate-300/25 bg-inherit px-4 py-8 align-middle xl:px-0">
       <div className="flex w-full max-w-7xl flex-col justify-center gap-4 px-4 md:flex-row md:justify-between">
         <div className="flex justify-center">
           <Image
@@ -14,12 +14,18 @@ export function Footer() {
             alt="logo sos pet"
           />
         </div>
-        <div className="flex flex-col gap-1 text-center align-middle md:flex-row md:gap-8">
+        <div className="flex flex-col items-center gap-1 text-center align-middle md:flex-row md:gap-8">
+          <Link
+            href="https://www.instagram.com/sospetbr/"
+            className="flex items-center gap-1 text-sm text-gray-500 hover:text-gray-700"
+          >
+            Siga-nos no Instagram <FaInstagram size={20} />
+          </Link>
           <Link
             href="mailto:sospet.suporte@gmail.com"
             className="text-sm text-gray-500 hover:text-gray-700"
           >
-            Suporte: sospet.suporte@gmail.com
+            Entre em contato: sospet.suporte@gmail.com
           </Link>
           <Link
             href="/terms-of-use"
@@ -35,6 +41,6 @@ export function Footer() {
           </Link>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
