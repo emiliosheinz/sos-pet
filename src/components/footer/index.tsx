2db3804da7b36fe5d75956d6252b31d0f4f97d83
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2024-05-11 16:20:26 -0300

    refactor: footer alignment

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,18 +1,32 @@
+import Image from "next/image";
+import { FaInstagram } from "react-icons/fa";
+
 export function Footer() {
   return (
-    <div className="relative bottom-0 mt-8 flex w-full justify-center border-t-2 border-slate-300/25 bg-inherit pb-8 pt-8">
-      <div className="flex w-4/5 flex-col items-center gap-1 sm:flex-row sm:justify-between">
-        <div>
+    <div className="mt-8 flex w-full justify-center border-t-2 border-slate-300/25 bg-inherit px-4 py-8 align-middle xl:px-0">
+      <div className="flex w-full max-w-7xl flex-col justify-center md:flex-row md:justify-between">
+        <div className="flex flex-col items-center justify-center gap-8 md:flex-row">
+          <Image
+            src="/logo-horizontal.svg"
+            width={80}
+            height={50}
+            alt="logo sos pet"
+          />
+          <FaInstagram size={20} />
+        </div>
+        <div className="flex flex-col gap-1 text-center align-middle md:flex-row md:gap-8">
           <a
             href="mailto:sospet.suporte@gmail.com"
-            className="text-sm text-gray-500"
+            className="text-sm text-gray-500 hover:text-gray-700"
           >
             Suporte: sospet.suporte@gmail.com
           </a>
-        </div>
-        <div className="flex flex-col gap-1 text-center align-middle sm:text-left md:flex-row md:gap-8">
-          <a className="text-sm text-gray-500">Termos de uso</a>
-          <a className="text-sm text-gray-500">Políticas de privacidade</a>
+          <a className="text-sm text-gray-500 hover:text-gray-700">
+            Termos de uso
+          </a>
+          <a className="text-sm text-gray-500 hover:text-gray-700">
+            Políticas de privacidade
+          </a>
         </div>
       </div>
     </div>
