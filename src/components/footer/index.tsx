fd1824e0d026cba89f5d7b981e6c903e3b47edfe
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2024-05-11 16:29:52 -0300

    feat: adds privacy policy and terms of use links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,32 +1,38 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FaInstagram } from "react-icons/fa";
 
 export function Footer() {
   return (
     <div className="mt-8 flex w-full justify-center border-t-2 border-slate-300/25 bg-inherit px-4 py-8 align-middle xl:px-0">
-      <div className="flex w-full max-w-7xl flex-col justify-center md:flex-row md:justify-between">
-        <div className="flex flex-col items-center justify-center gap-8 md:flex-row">
+      <div className="flex w-full max-w-7xl flex-col justify-center gap-4 md:flex-row md:justify-between">
+        <div className="flex justify-center">
           <Image
             src="/logo-horizontal.svg"
             width={80}
             height={50}
             alt="logo sos pet"
           />
-          <FaInstagram size={20} />
         </div>
         <div className="flex flex-col gap-1 text-center align-middle md:flex-row md:gap-8">
-          <a
+          <Link
             href="mailto:sospet.suporte@gmail.com"
             className="text-sm text-gray-500 hover:text-gray-700"
           >
             Suporte: sospet.suporte@gmail.com
-          </a>
-          <a className="text-sm text-gray-500 hover:text-gray-700">
+          </Link>
+          <Link
+            href="/terms-of-use"
+            className="text-sm text-gray-500 hover:text-gray-700"
+          >
             Termos de uso
-          </a>
-          <a className="text-sm text-gray-500 hover:text-gray-700">
+          </Link>
+          <Link
+            href="/privacy-policy"
+            className="text-sm text-gray-500 hover:text-gray-700"
+          >
             Políticas de privacidade
-          </a>
+          </Link>
         </div>
       </div>
     </div>
