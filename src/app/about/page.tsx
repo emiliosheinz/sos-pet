8385e18a6a1e9a311f559beb8b413bd004a1ce13
Guilherme Rigotti <ggrigotti@gmail.com>
2024-05-12 15:19:06 -0300

    Revert "refactor: update background color of About page and Footer component"
    This reverts commit 264031a3dc598446dc349cd9122ee9f72d1eeb46.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <main className="mb-8 bg-white">
+    <main className="mb-8">
       <div className="relative mb-8 h-72 w-full">
         <Image
           className="object-cover"
