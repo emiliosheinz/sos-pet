7f74414f7931068bfc3dc4ba156c5872c640649e
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-05-12 13:58:10 -0300

    fix: siginin

--- src/app/shelter/layout.tsx
+++ src/app/shelter/layout.tsx
@@ -1,11 +1,22 @@
+import { Loader2 } from "lucide-react";
 import { redirect } from "next/navigation";
-import { type PropsWithChildren } from "react";
+import { Suspense, type PropsWithChildren } from "react";
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function ShelterLayout({ children }: PropsWithChildren) {
   const session = await getServerAuthSession();
   if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/shelter");
+    redirect("/signin?callbackUrl=/shelter");
   }
-  return children;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex w-full justify-center pt-28">
+          <Loader2 className="size-8 animate-spin" />
+        </div>
+      }
+    >
+      {children}
+    </Suspense>
+  );
 }
