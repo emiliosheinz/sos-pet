c4b50af6daf334ecbcb1fb860c9a84da560e27f9
Guilherme Rigotti <ggrigotti@gmail.com>
2024-05-16 14:46:57 -0300

    refactor: Rename page component to Map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -7,7 +7,7 @@ import { api } from "~/trpc/react";
 
 const DEFAULT_LOCATION: LatLngTuple = [-30.0346, -51.2177]; // Porto Alegre
 
-export default function Page() {
+export default function Map() {
   const { data, isLoading } = api.shelter.findAll.useQuery();
   const [userLocation, setUserLocation] =
     useState<LatLngTuple>(DEFAULT_LOCATION);
