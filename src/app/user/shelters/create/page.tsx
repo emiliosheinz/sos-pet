277d45fd79888293b4503fa14e2ffd3ac3fa32b5
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-06-22 09:22:31 -0300

    feat: improve user shelters loading and performance

--- src/app/user/shelters/create/page.tsx
+++ src/app/user/shelters/create/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { FormEditRegister } from "~/app/user/shelters/_components";
 
 export default function CreateShelterPage() {
--- src/app/user/shelters/layout.tsx
+++ src/app/user/shelters/layout.tsx
@@ -1,22 +1,16 @@
-import { Loader2 } from "lucide-react";
-import { redirect } from "next/navigation";
-import { Suspense, type PropsWithChildren } from "react";
-import { getServerAuthSession } from "~/server/auth";
+"use client";
 
-export default async function ShelterLayout({ children }: PropsWithChildren) {
-  const session = await getServerAuthSession();
+import { useSession } from "next-auth/react";
+import { redirect, usePathname } from "next/navigation";
+import { type PropsWithChildren } from "react";
+
+export default function ShelterLayout({ children }: PropsWithChildren) {
+  const pathname = usePathname();
+  const { data: session } = useSession();
+
   if (!session) {
-    redirect("/signin?callbackUrl=/user/shelters");
+    redirect(`/signin?callbackUrl=${pathname}`);
   }
-  return (
-    <Suspense
-      fallback={
-        <div className="flex w-full justify-center pt-28">
-          <Loader2 className="size-8 animate-spin" />
-        </div>
-      }
-    >
-      {children}
-    </Suspense>
-  );
+
+  return children;
 }
